5064913beee5204b059c290af47348d3954c2399
Rohan Patel <42444158+gamble4846@users.noreply.github.com>
2023-01-28 18:10:59 +0530

    .

---
+++ Breadcrumb/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'breadcrumb-303115',
+    appId: '1:612986271839:web:f0e79c47838430836f43ea',
+    storageBucket: 'breadcrumb-303115.appspot.com',
+    apiKey: 'AIzaSyB-cDmq8pLuBhWFAC5GlEnLFItO_yjaYFg',
+    authDomain: 'breadcrumb-303115.firebaseapp.com',
+    messagingSenderId: '612986271839',
+  },};
