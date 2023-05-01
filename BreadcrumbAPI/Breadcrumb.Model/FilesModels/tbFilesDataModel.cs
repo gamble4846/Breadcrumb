e7838a91a9462d18dcb7148862b84359a84ced99
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2023-05-01 17:17:57 +0530

    dcs

--- BreadcrumbAPI/Breadcrumb.Model/FilesModels/tbFilesDataModel.cs
+++ BreadcrumbAPI/Breadcrumb.Model/FilesModels/tbFilesDataModel.cs
@@ -13,5 +13,6 @@ namespace Breadcrumb.Model.FilesModels
         public string Type { get; set; }
         public string Name { get; set; }
         public string ThumbnailLink { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
--- BreadcrumbAPI/Breadcrumb.Model/FilesModels/tbFilesDataViewModel.cs
+++ BreadcrumbAPI/Breadcrumb.Model/FilesModels/tbFilesDataViewModel.cs
@@ -12,5 +12,6 @@ namespace Breadcrumb.Model.FilesModels
         public string Type { get; set; }
         public string Name { get; set; }
         public string ThumbnailLink { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
